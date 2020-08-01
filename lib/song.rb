9a67f2507d57a1abaa3b801150f2dcec2375a232
Nick Lanese <nlanese21@gmail.com>
2020-08-01 02:13:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,74 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    newSong = Song.new
+    newSong.save
+    return newSong
+  end
+
+  def self.new_by_name(name)
+    newSong = Song.new
+    newSong.name=(name)
+    return newSong
+  end
+
+  def self.create_by_name(name)
+    newSong = Song.new
+    newSong.name=(name)
+    newSong.save
+    return newSong
+  end
+
+  def self.find_by_name(name)
+    @@all.find{ | songName | name == songName.name}
+  end
+
+  def self.find_or_create_by_name(name)
+    hasAlready = false
+    @@all.each do | song |
+      if (song.name == name)
+        hasAlready = true
+      end
+    end
+    if (hasAlready)
+      Song.find_by_name(name)
+    else
+      Song.create_by_name(name)
+    end
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  def self.new_from_filename( filename )
+    fileNameArray = filename.split(" - ")
+    @artist_name = fileNameArray[0]
+    fileNameArray = fileNameArray.shift
+    return fileNameArray
+  end
+
+  def self.alphabetical
+    i = 0
+    rArray = []
+    songArray = @@all
+    topValue = @@all.length
+    until (i == topValue) do
+      first = "zzz"
+      chosen = ""
+      songArray.each do | song |
+        if (song.name < first)
+          first = song.name
+          chosen = song
+        end
+      end
+      rArray << chosen
+      i = i + 1
+      deleteVal = self.find_by_name(first)
+      songArray.delete(deleteVal)
+    end
+    return rArray
+  end
+
 end
